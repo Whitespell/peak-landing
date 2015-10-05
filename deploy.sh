4e1a403564858af915cac4ec98ee394f2d30bbcb
Koen Vendrik <k.vendrik@gmail.com>
2015-10-05 20:39:07 +0200

    Enhances deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Starting deployment";
 ssh website-internal.whitespell.com "sudo mkdir -p /var/www/peakapp.me && sudo chmod -R 777 /var/www/peakapp.me";
-scp -r www/* website-internal.whitespell.com:/var/www/peakapp.me;
+rsync -av www/ website-internal.whitespell.com:/var/www/peakapp.me;
 ssh website-internal.whitespell.com "sudo chmod -R 755 /var/www/peakapp.me";
 echo "Deployment Successful";
